2618003240426a53fd82d8b61a6acc86c99209ee
tastytea <tastytea@tastytea.de>
2022-05-24 21:11:46 +0200

    */*: remove www-apps/pleroma, user, group

--- www-apps/pleroma/files/syslog.exs
+++
@@ -1,8 +0,0 @@
-config :logger,
-  backends: [{ExSyslogger, :ex_syslogger}]
-
-config :logger, :ex_syslogger,
-  level: :warn,
-  ident: "pleroma",
-  format: "$metadata[$level] $message",
-  metadata: [:request_id]
